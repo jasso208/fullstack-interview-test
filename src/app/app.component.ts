6466aec5e31868db2a478e503ac4d4eb75f4ab80
jasso208 <jasso.gallegos@gmail.com>
2021-07-03 17:47:32 -0500

    Agregamos formulario para indicar repositorio GitHub a consultar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'fullstack-interview-test';
+
+  public formRepositorio:FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.formRepositorio = fb.group(
+      {
+        nombreRepositorioGithub : new FormControl()
+      }
+    );
+  }
+
+  ngOnInit(){
+    this.formRepositorio.get("nombreRepositorioGithub")?.setValue(localStorage.getItem("nombreRepositorioGitHub"));
+  }
+
+  cambiaRepositorioGitHub(){
+    let nombreRepositorioGithub= this.formRepositorio.get("nombreRepositorioGithub")?.value;
+    localStorage.setItem("nombreRepositorioGitHub",nombreRepositorioGithub);
+    window.location.reload();
+  }
+
+  
+
+
 }
